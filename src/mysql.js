ba40f1ba683700dff33a2d38b0a27ec3c2f52ef2
cheng403003167 <403003167@qq.com>
2020-01-21 16:05:48 +0800

    a

--- src/mysql.js
+++ src/mysql.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: '3306',
   user: 'root',
-  password: '3203550Cheng',
+  password: '',
   database: 'nitu_data'
 });
 class mysqlClass {
